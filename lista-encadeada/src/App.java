1fb31dbf53b679b204f8540f4b238c4d57707d51
Anderson Amantino <andersonamantino@gmail.com>
2023-03-03 20:56:43 -0300

    fix: methods

--- lista-encadeada/src/App.java
+++ lista-encadeada/src/App.java
@@ -1,5 +1,23 @@
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        ListaEncadeada <String> minhaListaEnc1 = new ListaEncadeada<>();
+
+        minhaListaEnc1.add("primeiro index");
+        minhaListaEnc1.add("segundo index");
+        minhaListaEnc1.add("terceiro index");
+        minhaListaEnc1.add("quarto index");
+        
+
+        System.out.println(minhaListaEnc1.get(0));
+        System.out.println(minhaListaEnc1.get(1));
+        System.out.println(minhaListaEnc1.get(2));
+        System.out.println(minhaListaEnc1.get(3));
+
+        System.out.println("????"+minhaListaEnc1.toStringHandmade());
+        minhaListaEnc1.remove(2);
+        System.out.println("||||" + minhaListaEnc1.toStringHandmade());
     }
 }
