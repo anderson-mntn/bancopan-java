3894c4d785dedf006a5cceaa4176bc66e82bb7aa
Anderson Amantino <andersonamantino@gmail.com>
2023-03-07 15:21:04 -0300

    add: methods

--- No-Arvore/src/dio/ArvoreBinaria.java
+++ No-Arvore/src/dio/ArvoreBinaria.java
@@ -22,4 +22,116 @@ public class ArvoreBinaria<T extends Comparable<T>> {
         }
         return atual;
     }
+
+    public void exibirInOrder(){
+        System.out.println("\n Exibindo IN-ORDER:");
+        exibirInOrder(this.raiz);
+    }
+
+    private void exibirInOrder(BinNo<T> atual){
+        if(atual != null){
+            exibirInOrder(atual.getLeftNo());
+            System.out.println(atual.getConteudo() + ", ");
+            exibirInOrder(atual.getRightNo());
+        } 
+    }
+
+    public void exibirPosOrder(){
+        System.out.println("\n Exibindo POS-ORDER:");
+        exibirPosOrder(this.raiz);
+    }
+
+    private void exibirPosOrder(BinNo<T> atual){
+        if(atual != null){
+            exibirPosOrder(atual.getLeftNo());
+            exibirPosOrder(atual.getRightNo());
+            System.out.println(atual.getConteudo() + ", ");
+        } 
+    }
+
+    public void exibirPreOrder(){
+        System.out.println("\n Exibindo PRE-ORDER:");
+        exibirPreOrder(this.raiz);
+    }
+
+    private void exibirPreOrder(BinNo<T> atual){
+        if(atual != null){
+            System.out.println(atual.getConteudo() + ", ");
+            exibirPreOrder(atual.getLeftNo());
+            exibirPreOrder(atual.getRightNo());
+        } 
+    }
+
+    public void remover(T conteudo){
+        try {
+            BinNo<T> atual = this.raiz;
+            BinNo<T> pai = null;
+            BinNo<T> filho = null;
+            BinNo<T> temp = null;
+
+            while(atual != null && !atual.getConteudo().equals(conteudo)){
+                pai = atual;
+                if(conteudo.compareTo(atual.getConteudo()) < 0){
+                    atual = atual.getLeftNo();
+                } else {
+                    atual = atual.getRightNo();
+                }
+            }
+
+            if(atual == null){
+                System.out.println("Conteudo nao encontrado - Bloco try.");
+            }
+
+            if(pai == null){
+                if(atual.getRightNo() == null){
+                    this.raiz = atual.getLeftNo();
+                } else if(atual.getLeftNo() == null){
+                    this.raiz = atual.getRightNo();
+                } else {
+                    for(temp = atual, filho = atual.getLeftNo();
+                    filho.getRightNo() != null;
+                    temp = filho, filho = filho.getLeftNo()){
+                        if(filho != atual.getLeftNo()){
+                            temp.setRightNo(filho.getLeftNo());
+                            filho.setLeftNo(raiz.getLeftNo());
+                        }
+                    }
+                    filho.setRightNo(raiz.getRightNo());
+                    raiz = filho;
+                }
+            } else if(atual.getRightNo() == null){
+                if(pai.getLeftNo() == atual){
+                    pai.setLeftNo(atual.getLeftNo());
+                } else {
+                    pai.setRightNo(atual.getLeftNo());
+                }
+            } else if (atual.getLeftNo() == null){
+                if(pai.getLeftNo() == atual){
+                    pai.setLeftNo(atual.getRightNo());
+                } else {
+                    pai.setRightNo(atual.getRightNo());
+                }
+            } else {
+                for(
+                    temp = atual, filho = atual.getLeftNo();
+                    filho.getRightNo() != null;
+                    temp = filho, filho = filho.getRightNo()
+                ){
+                    if(filho!= atual.getLeftNo()){
+                        temp.setRightNo(filho.getLeftNo());
+                        filho.setLeftNo(atual.getLeftNo());
+                    }
+                    filho.setRightNo(atual.getRightNo());
+                    if(pai.getLeftNo()== atual){
+                        pai.setLeftNo(filho);
+                    } else {
+                        pai.setRightNo(filho);
+                    }
+                }
+            }
+
+        } catch (NullPointerException error) {
+            System.out.println("Conteudo nao encontrado - Bloco catch.");
+        }
+    }
 }
---
+++ No-Arvore/src/dio/model/Obj.java
@@ -0,0 +1,36 @@
+package dio.model;
+
+public class Obj extends ObjArvore<Obj> {
+
+    Integer meuValor;
+
+    public Obj(Integer meuValor){
+        this.meuValor = meuValor;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'equals'");
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'hashCode'");
+    }
+
+    @Override
+    public int compareTo(Obj outro) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'compareTo'");
+    }
+
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'toString'");
+    }
+    
+}
