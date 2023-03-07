5ecf4bfe434d42b3b110615c9cbb2c9ed9de05fc
Anderson Amantino <andersonamantino@gmail.com>
2023-03-07 20:39:13 -0300

    .

--- equals-e-hashcode/src/dio/Main.java
+++ equals-e-hashcode/src/dio/Main.java
@@ -1,8 +1,12 @@
 package dio;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
+
+import org.w3c.dom.stylesheets.LinkStyle;
 
 
 public class Main {
@@ -31,5 +35,36 @@ public class Main {
         System.out.println(stackCarros.pop());
         System.out.println(stackCarros.peek());
         System.out.println(stackCarros.empty());
+
+        // -------- Queue (Fila, LikedList) --------
+
+        Queue<Carro> queueCarros = new LinkedList<>();
+        queueCarros.add(new Carro("Ford"));
+        queueCarros.add(new Carro("Fiat"));
+        queueCarros.add(new Carro("Chevrolet"));
+
+        System.out.println(queueCarros);
+        System.out.println(queueCarros.offer(new Carro("Renault"))); //retorna false se não conseguir adicionar
+        System.out.println(queueCarros.peek()); // primeiro da fila, se lista estiver vazia retorna null.
+        System.out.println(queueCarros.poll()); // remove o primeiro
+        System.out.println(queueCarros.size());
+
+        // ---------- List (ArrayList) ---------
+
+        List<Carro> listaCarros = new ArrayList<>();
+
+        listaCarros.add(new Carro("BMW"));
+        listaCarros.add(new Carro("FERRARI"));
+        listaCarros.add(new Carro("MERCEDEZ"));
+        listaCarros.add(new Carro("FORD"));
+
+        System.out.println(listaCarros.contains(new Carro("FORD")));
+        System.out.println(listaCarros.get(2)); // pega pelo index
+        System.out.println(listaCarros.indexOf(new Carro("MERCEDEZ")));
+        listaCarros.remove(2);
+        System.out.println(listaCarros);
+
+
+
     }  
 }
---
+++ set/src/dio/Main.java
@@ -0,0 +1,30 @@
+package dio;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Set<Carro> hashSetCarros = new HashSet<>();
+
+        hashSetCarros.add(new Carro("Ford"));
+        hashSetCarros.add(new Carro("Fiat"));
+        hashSetCarros.add(new Carro("Peugeot"));
+        hashSetCarros.add(new Carro("Porsche"));
+        hashSetCarros.add(new Carro("Alfa Romeo"));
+
+        System.out.println(hashSetCarros); // Set nao preserva ordem de inserção
+
+        Set<Carro> treeSetCarros = new TreeSet<>();
+
+        treeSetCarros.add(new Carro("Ford"));
+        treeSetCarros.add(new Carro("Fiat"));
+        treeSetCarros.add(new Carro("Peugeot"));
+        treeSetCarros.add(new Carro("Porsche"));
+        treeSetCarros.add(new Carro("Alfa Romeo"));
+
+        System.out.println(treeSetCarros); // Vai dar erro, precisamos implementar comparable na class Carro
+    }  
+}
